6a9b5c173d15e4b535c98bcf3c1b2c4548c264ef
Bralen Bailey <118554238+Bralen@users.noreply.github.com>
2025-01-20 22:11:43 -0700

    Add files via upload

--- Module_3_Python.py
+++ Module_3_Python.py
@@ -1,22 +1,27 @@
-checkTotal = float(input('Enter the charge for the food.'))
+checkList = []
+checkList.append(float(input('Enter the charge for the food.')))
 #Any gratuity that is automatically imposed is considered a service fee and is subject to sales tax.
-gratuityAdded = checkTotal * 1.18
-salesTaxAdded = gratuityAdded * 1.07
-print('The total with gratuity added is $' + str(round(gratuityAdded,2)))
-print('The total with gratuity and sales tax added is $' + str(round(salesTaxAdded,2)))
+checkList.append(checkList[0] * 0.18)
+checkList.append((checkList[0] + checkList[1]) * 0.07)
+print('The amount of gratuity is ' + str(checkList[1]))
+print('The amount of sales tax is ' + str(checkList[2]))
+checkTotal = 0
+counter = 0
+while counter < len(checkList):
+    checkTotal = checkTotal + checkList[counter]
+    counter += 1
+print('The total with gratuity and sales tax added is $' + str(round(checkTotal,2)))
 
-currentTime = float(input('Enter the current time in hours. '))
-waitTime = float(input('Enter how long the alarm should wait. '))
+timeList = []
+timeList.append(float(input('Enter the current time in hours. ')))
+timeList.append(float(input('Enter how long the alarm should wait. ')))
 
-if (currentTime + waitTime) < 24:
-    timeToSound = currentTime + waitTime
-else:
-    timeToSound = (currentTime + waitTime) % 24
+counter = 0 
+while counter <= timeList[1]:
+    timeList[0]+=1
+    if timeList[0] == 25:
+        timeList[0] = 0
+    counter += 1
 
-if timeToSound < 12:
-    timeOfDay = 'AM'
-else:
-    timeOfDay = 'PM'
-
-print('The alarm will sound at ' + str(int(timeToSound)) + timeOfDay)
+print('The alarm will sound at ' + str(int(timeList[0])))
 
