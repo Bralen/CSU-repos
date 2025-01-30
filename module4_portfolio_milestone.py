3132d239d05413ffdac895a9945573396e27adbc
Bralen Bailey <118554238+Bralen@users.noreply.github.com>
2025-01-29 21:02:47 -0700

    Add files via upload

--- module4_portfolio_milestone.py
+++ module4_portfolio_milestone.py
@@ -10,18 +10,25 @@ class ItemToPurchase:
 
 def main():
     counter = 0
+    itemList = []
     while counter < 2:   
         nameVar = str(input("Enter the item name"))
         priceVar = float(input("Enter the item price"))
         qtyVar = int(input("Enter the item quantity"))
         if counter == 0:
             item1 = ItemToPurchase(nameVar, priceVar, qtyVar)
+            itemList.append(item1)
         else:
              item2 = ItemToPurchase(nameVar, priceVar, qtyVar)
+             itemList.append(item2)
         counter += 1
-    item1.print_item_cost()
-    item2.print_item_cost()
-    print("Total: $" + str((item1.quantity * item1.price) + (item2.quantity * item2.price)))
+    totalCost = 0
+    for item in itemList:
+        item.print_item_cost()
+        totalCost = totalCost + (item.quantity * item.price)
+    print("Total: $" + str(totalCost))
+
+    #print("Total: $" + str((item1.quantity * item1.price) + (item2.quantity * item2.price)))
 
 main()
 
